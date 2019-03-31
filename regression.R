e157b9c762f59cd0fab245aaa2a73328e06c4102
rheilmayr <rheilmayr@gmail.com>
2019-03-31 14:20:06 -0700

    Quick hacking with Fran to outline two-stage analysis

--- regression.R
+++ regression.R
@@ -1,8 +1,11 @@
 install.packages("plm")
+install.packages("lfe")
 
 library(tidyverse)
-library(plm)
+library(lfe)
+library(broom)
 
+library(ggiraphExtra)
 ### Define path
 wdir = 'D:/cloud/Google Drive/Treeconomics/Data/'
 setwd(wdir)
@@ -13,7 +16,7 @@ df <- read.csv(paste0(wdir,spp,'_data.csv'))
 df <- df %>%
   mutate(tree_id = paste0(site_id, tree_id))
 
-### Calculate deviation in growth rate
+### Calculate deviation in growth rate                     ## Alternative is polynomial (logitic) age control interacted with site in single stage regression
 find_deviation <- function(df) {
   fit <- lm(ring_width ~ age, data = df)                   ## Replace with logistic growth function
   df$predict <- fit %>% predict(type = "response")
@@ -23,12 +26,14 @@ find_deviation <- function(df) {
 
 df$deviation <- find_deviation(df)
 
-df$deviation <- df %>%                              ## Ask Fran about this - How to run function by site?
+df %>%                              ## Rewrite to run function by site
   group_by(site_id) %>%
-  do(find_deviation)
+  summarise(deviation = find_deviation())
+  do(site_dev = find_deviation(.))
 
 ### Calculate site average aet and cwd
 site_clim <- df %>%
+  filter(year < 1980) %>%                                                                      ## Switch to raw climate data rather than using tree dataframe
   group_by(site_id, year) %>%
   summarize(cwd.an = max(cwd.an, na.rm = TRUE), aet.an = max(aet.an, na.rm = TRUE)) %>%
   filter((cwd.an > -Inf) & (aet.an > -Inf)) %>%
@@ -37,16 +42,29 @@ site_clim <- df %>%
 
 df <- merge(x = df, y = site_clim, by = "site_id", all.x = TRUE)
 
+### CWD data binned                                            ## Probably not necessary since our CWD/AET variables are already integrals
+df <- cut(x, breaks = quantile(x, probs = seq(0, 1, 0.1)), 
+          labels = 1:10, include.lowest = TRUE)
+
 # Climate regressions
-mod <- lm(deviation ~ cwd.an + aet.an, data = df)
-summary(mod)
-
-mod_squared <- lm(deviation ~ cwd.an + I(cwd.an^2) + aet.an + I(aet.an^2), data = df)
-summary(mod_squared)
-
-
-mod_interact <- lm(deviation ~ cwd.ave * cwd.an + cwd.ave * I(cwd.an^2), data = df)
+mod_interact <- felm(deviation ~ cwd.ave * cwd.an + cwd.ave * I(cwd.an^2)-cwd.ave|site_id|0|site_id, data = df )
 summary(mod_interact)
 
-mod_interact <- lm(deviation ~ cwd.ave * cwd.an, data = df)
+mod_interact <- felm(deviation ~ aet.ave * aet.an + aet.ave * I(aet.an^2)-aet.ave|site_id|0|site_id, data = df )
 summary(mod_interact)
+
+complete_df <- df[which(complete.cases(df[,c(7,10)])),]
+
+site_lm <- complete_df %>% 
+  group_by(site_id) %>%
+  do(fit_site = lm(deviation ~ cwd.an, data = . ))
+siteCoef = tidy(site_lm, fit_site) %>%
+  filter(term == "cwd.an")
+siteCoef=merge(siteCoef,unique(complete_df[,c(1,11)]),all.x=T,all.y=F)
+grandmodel=lm(estimate~cwd.ave+I(cwd.ave^2),data=siteCoef)
+grandmodel=lm(estimate~cwd.ave*I(cwd.ave>250)-I(cwd.ave>250),data=siteCoef)
+
+# Explore regression results
+x = 0:1000
+plot(x,x*mod_interact$coefficients[1]+x^2*mod_interact$coefficients[2])
+waldtest(mod_interact,c(F,F,T,T))
