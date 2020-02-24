75f2e8e6609a5f0889fba3457061bd5559dba47b
rheilmayr <rheilmayr@gmail.com>
2020-02-24 10:20:26 -0800

    Expanding to top 20 species. Cleaning up and annotating.

--- db_access.R
+++ db_access.R
@@ -3,10 +3,9 @@ library(dbplyr)
 library(RSQLite)
 
 # Define path
-wdir = 'C:/Users/rheil/Google Drive/Treeconomics/Data/'
-#for Fran
-wdir="C:/Users/fmoor/Google Drive/Treeconomics/Data/"
 wdir = 'D:/cloud/Google Drive/Treeconomics/Data/'
+# #for Fran
+# wdir="C:/Users/fmoor/Google Drive/Treeconomics/Data/"
 tree_db = paste0(wdir, 'tree_ring_data_V2.db')
 cwd_csv = paste0(wdir, 'essentialcwd_data.csv')
 
@@ -15,30 +14,25 @@ sqlite <- dbDriver("SQLite")
 conn <- dbConnect(sqlite, tree_db)
 tables = dbListTables(conn)
 
-# Pull all tree records for top 6 species
-spp=c("psme","pipo","pisy","pcgl","pcab","pied")
-speciesnames=c("Douglas Fir","Ponderosa Pine","Scotch Pine","White Spruce","Norway Spruce","Colorado Pinyon")
-
-#collect trees from identified species
+# Open tables
 tree_db = as.data.frame(tbl(conn, 'trees'))
+spp_db = as.data.frame(tbl(conn,'species'))
+
+# Pull all tree records for top 6 species
+#collect trees from identified species
+count_spp <- tree_db %>% 
+  group_by(species_id) %>%
+  tally() %>%
+  arrange(-n)
+
+spp = count_spp[1:20,] %>% pull(species_id)
+spp_names = spp_db %>%
+  filter(species_id %in% spp) %>%
+  as_tibble()
+
+# ID trees from selected species
 tree_ids = tree_db %>%
   filter(species_id %in% spp) %>%
-
-  # Identify all trees of a given species
-# Hypotheses: 1) quercus, sugarpine, cedar, white fir, juniper, redwood would exhibit spoiled tree
-# High elevation tree species will exhibit less of an effect because high cwd is correlated with release from growth constraints 
-species_ids = tree_db %>%
-  select(species_id) %>%
-  collect() %>%
-  group_by(species_id) %>%
-  count() %>%
-  arrange(desc(n))
-
-spp = 'pipo' #Whitebark = pial; douglas fir = 'psme'; ponderosa = 'pipo'
-tree_db = tbl(conn, 'trees')
-
-tree_ids = tree_db %>%
-  filter(species_id == spp) %>%
   select('tree_id') %>%
   collect()
 
@@ -50,7 +44,7 @@ obs_db = obs_db %>%
 obs = obs_db %>%
   collect()
 
-# Load and join cwd data
+# Load and combine cwd data
 cwd_df = read.csv(cwd_csv, sep=',')
 cwd_df.groups = cwd_df %>%
   group_by(site, year)
@@ -63,24 +57,24 @@ clim.join = aet.annual %>%
 
 # Pull tree obsevations
 obs_db = as.data.frame(tbl(conn, 'observations_new'))
-obs_species=data.frame()
-for(i in 1:length(spp)){
-  trees=tree_ids %>%
-    filter(species_id==spp[i]) %>%
-    select(tree_id)
-  obs = obs_db %>%
-    filter(tree_id %in% trees$tree_id) %>%
-    arrange(tree_id, desc(year))
-  obs = obs %>%
-    collect()
-  obs.join = obs %>%
-    inner_join(clim.join, by = c("site_id" = "site", "year" = "year")) #note that we loose a few sites because we are missing CWD data - probably becaue they are on the coast and more sites because we don't have cwd data before 1900
-  obs.join$species=rep(spp[i],dim(obs.join)[1])
-  obs_species=rbind(obs_species,obs.join)
-  print(i)
-}
+obs = obs_db %>%
+  filter(tree_id %in% pull(tree_ids)) %>%
+  arrange(tree_id, desc(year))%>%
+  collect()
 
-obs_species$age=obs_species$year-obs_species$first_year
-write.csv(obs_species, file = paste0(wdir,'topsixspecies_data.csv'), row.names = FALSE)
+# Add climate data
+obs = obs %>%
+  inner_join(clim.join, by = c("site_id" = "site", "year" = "year")) #note that we loose a few sites because we are missing CWD data - probably becaue they are on the coast and more sites because we don't have cwd data before 1900
+
+# Add species labels
+obs <- obs %>%
+  inner_join(tree_db, by = c("tree_id", "site_id"))
+
+obs <- obs %>%
+  inner_join(spp_names, by = 'species_id')
+
+# Write to shared directory
+obs$age=obs$year-obs$first_year
+write.csv(obs, file = paste0(wdir,'species_data.csv'), row.names = FALSE)
 
 
