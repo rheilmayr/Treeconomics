ec84d93fed7e2e5227396b9009ef429c9349e667
rheilmayr <rheilmayr@gmail.com>
2020-02-24 16:26:20 -0800

    Hacking session at UCD

--- regression.R
+++ regression.R
@@ -1,3 +1,15 @@
+# TODO:
+#   Bootstrap first stage
+#   Look at temperature and latitude in second stage regression (pure cwd in first stage)
+#   Add measure of species distance from cwd mean - standardized metric of cwd.ave
+#   add lagged climate data maybe up to 2 years
+#   Hardwood vs softwood
+#   Do you get snow?
+#   PET in first stage (keep this separate from cwd model)
+#   Try out single-stage approach - panel model with non-linear terms  
+  
+
+
 #install.packages("plm")
 #install.packages("lfe")
 
@@ -78,6 +90,7 @@ site_lm <- complete_df %>%
   group_by(site_id, species_id) %>%
   do(fit_site = felm(ring_width ~ cwd.an+age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = . ))
   # do(fit_site = felm(ring_width ~ cwd.an +aet.an+age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = . ))
+  # do(fit_site = felm(ring_width ~ cwd.an +pet.an+age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = . ))
 
 siteCoef=tidy(site_lm[[3]][[1]])%>%filter(term=="cwd.an")
 for(i in 2:length(site_lm[[3]])){
@@ -129,7 +142,8 @@ xlim.all <- site_summary %>%
 # a=ggplot(siteCoef_trimmed,aes(x=cwd.ave,y=estimate,col=species))+theme_bw()+geom_point()+facet_wrap(~species)
 # a=a+labs(x="Average Climatic Water Deficit",y="Marginal Effect of CWD")+scale_color_discrete(guide=FALSE)
 
-kd <- kde2d(x = site_summary$cwd.ave, y = site_summary$aet.ave, n = 25, lims = c(c(0,750),c(100, 600)))
+kd <- kde2d(x = site_summary$cwd.ave, y = site_summary$pet.ave, n = 25, lims = c(c(0,750),c(100, 600)))
+points(site_summary$cwd.ave, site_summary$pet.ave)
 filled.contour(kd, color.palette = pal)
 
 x11()
@@ -141,7 +155,8 @@ for(i in 1:dim(xlim.sp)[1]){
   # kd <- kde2d(x = dat$cwd.ave, y = dat$pet.ave, n = 25, lims = c(c(lim$cwd.qlow, lim$cwd.qhigh), c(lim$pet.qlow, lim$pet.qhigh)))
   kd <- kde2d(x = dat$cwd.ave, y = dat$aet.ave, n = 25, 
               lims = c(c(xlim.all$cwd.qlow,xlim.all$cwd.qhigh),c(xlim.all$aet.qlow, xlim.all$aet.qhigh)))
-  contour(kd)
+  filled.contour(kd, color.palette = pal)
+  title(paste0(spec_name, dim(dat)[1]))
   # kde2d(x, y, h, n = 25, lims = c(range(x), range(y)))
 }
 
@@ -150,34 +165,40 @@ dat = siteCoef_trimmed %>%
   ungroup() %>%
   drop_na(std.error) %>%
   mutate(errorweights=1/std.error/sum(1/std.error,na.rm=T)) 
-grandmodel <- lm(estimate~cwd.ave+I(cwd.ave^2) + aet.ave + I(aet.ave^2),weights=errorweights,data= dat)
+grandmodel <- lm(estimate~cwd.ave+I(cwd.ave^2) + I(cwd.ave^3) + pet.ave + I(pet.ave^2) + I(pet.ave^3),weights=errorweights,data= dat)
 # grandmodel <- lm(estimate~cwd.ave+I(cwd.ave^2), weights=errorweights,data= dat)
 
 cwd.x <- seq(xlim.all$cwd.qlow, xlim.all$cwd.qhigh, length.out=1000)
 aet.med <- xlim.all$aet.qmed
+pet.med <- xlim.all$pet.qmed
 
-pred <- predict(grandmodel,data.frame(cwd.ave=cwd.x, aet.ave = aet.med),interval="prediction",level=0.90)
+pred <- predict(grandmodel,data.frame(cwd.ave=cwd.x, pet.ave = pet.med),interval="prediction",level=0.90)
 pred <- cbind(pred,cwd.x)
 pred <- pred %>% as_tibble()
 plot(cwd.x, pred$fit, type="l",xlab="Historic CWD",ylab="Estimated effect of CWD",las=1,lwd=2,col="#0dcfca",
      ylim = c(pred$lwr %>% min(), pred$upr %>% max()))
+points(dat$cwd.ave,dat$estimate,col=dat$species_id,pch=18)
 polygon(c(cwd.x,rev(cwd.x)),c(pred$lwr,rev(pred$upr)),col=rgb(t(col2rgb("#0dcfca")),max=255,alpha=70),border=NA)
 
-# aet.x <- seq(xlim.all$aet.qlow, xlim.all$aet.qhigh, length.out=1000)
-# cwd.med <- xlim.all$cwd.qmed
-# pred <- predict(grandmodel,data.frame(cwd.ave=cwd.med, aet.ave = aet.x),interval="prediction",level=0.90)
-# pred <- cbind(pred,aet.x)
-# pred <- pred %>% as_tibble()
-# plot(aet.x, pred$fit, type="l",xlab="Historic AET",ylab="Marginal effect of AET",las=1,lwd=2,col="#0dcfca")
+
+pet.x <- seq(xlim.all$pet.qlow, xlim.all$pet.qhigh, length.out=1000)
+cwd.med <- xlim.all$cwd.qmed
+
+pred <- predict(grandmodel,data.frame(cwd.ave=cwd.med, pet.ave = pet.x),interval="prediction",level=0.90)
+pred <- cbind(pred,pet.x)
+pred <- pred %>% as_tibble()
+plot(pet.x, pred$fit, type="l",xlab="Historic PET",ylab="Marginal effect of CWD",las=1,lwd=2,col="#0dcfca")
+points(dat$pet.ave,dat$estimate,add=T)
+polygon(c(pet.x,rev(pet.x)),c(pred$lwr,rev(pred$upr)),col=rgb(t(col2rgb("#0dcfca")),max=255,alpha=70),border=NA)
 
 
 #### Run species specific second stage ####
 grandmodels=siteCoef_trimmed %>%
   group_by(species_name) %>%
   drop_na(std.error) %>%
-  mutate(errorweights=1/std.error/sum(1/std.error,na.rm=T)) %>%
-  do(speciesmod=lm(estimate~cwd.ave+I(cwd.ave^2),weights=errorweights,data= .))
-  # do(speciesmod=lm(estimate~cwd.ave + I(cwd.ave^2) + aet.ave + I(aet.ave^2),weights=errorweights,data= .))
+  mutate(errorweights=1/std.error/sum(1/std.error,na.rm=T)) %>% ## LETS RECONSIDER THIS 
+  # do(speciesmod=lm(estimate~cwd.ave+I(cwd.ave^2)+I(cwd.ave^3),weights=errorweights,data= .))
+  do(speciesmod=lm(estimate~cwd.ave + I(cwd.ave^2) + pet.ave + I(pet.ave^2),weights=errorweights,data= .))
 
 # Review results
 grandmodels %>% filter(species_name=="blue oak") %>% pull(speciesmod)
@@ -187,10 +208,11 @@ predictions=data.frame()
 for(i in 1:dim(grandmodels)[1]){
   species=as.data.frame(grandmodels)[i,1]
   cwd.x=seq(xlim.sp$cwd.qlow[which(xlim.sp$species_name==species)],xlim.sp$cwd.qhigh[which(xlim.sp$species_name==species)],length.out=1000)
-  aet.x=seq(xlim.sp$aet.qlow[which(xlim.sp$species_name==species)],xlim.sp$aet.qhigh[which(xlim.sp$species_name==species)],length.out=1000)
-  prd=predict(grandmodels[[2]][[i]],data.frame(cwd.ave=cwd.x),interval="prediction",level=0.90)
+  pet.x=seq(xlim.sp$pet.qlow[which(xlim.sp$species_name==species)],xlim.sp$pet.qhigh[which(xlim.sp$species_name==species)],length.out=1000)
+  prd=predict(grandmodels[[2]][[i]],data.frame(cwd.ave=cwd.x, pet.ave = xlim.sp$aet.qmed[which(xlim.sp$species_name==species)]),interval="prediction",level=0.90)
   # prd=predict(grandmodels[[2]][[i]],data.frame(cwd.ave=cwd.x, aet.ave=xlim.sp$aet.qmed[which(xlim.sp$species_name==species)]),interval="prediction",level=0.90)
-  predictions=rbind(predictions,cbind(prd,cwd.x,aet.x))
+  # prd=predict(grandmodels[[2]][[i]],data.frame(cwd.ave=xlim.sp$cwd.qmed[which(xlim.sp$species_name==species)], aet.ave=aet.x),interval="prediction",level=0.90)
+  predictions=rbind(predictions,cbind(prd,cwd.x,pet.x))
 }
 predictions$species=rep(as.data.frame(grandmodels)[,1],each=length(cwd.x))
 # x11()
