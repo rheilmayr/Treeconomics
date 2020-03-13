1fcafa509f4c52285b6c61283e43f30aefee209e
rheilmayr <rheilmayr@gmail.com>
2020-03-13 16:03:06 -0700

    1) standardized historic climate by species 2) tweaks to first stage - adding pet 3) making a mess of code

--- regression.R
+++ regression.R
@@ -57,6 +57,7 @@ site_clim <- df %>%
 
 df <- merge(x = df, y = site_clim, by = "site_id", all.x = TRUE)
 
+
 #remove sites with negative ages
 df_trim <- df %>%
   filter(age>0)
@@ -77,40 +78,139 @@ df_trim=left_join(df_trim,ntrees, by = c("site_id", "species_id"))
 df_trim=df_trim%>%
   filter(ntrees>5)
 
-complete_df=df %>%
-  drop_na(c("cwd.an","aet.an","ring_width"))
+complete_df <- df_trim %>%
+  drop_na(c("cwd.an","aet.an","pet.an", "ring_width"))
 
 site_summary <- complete_df %>% 
   select("site_id", "species_id", "species_name", "cwd.ave", "pet.ave", "aet.ave") %>%
   distinct() %>%
   as_tibble()
 
+#### Calculate standardized climate variables for each species ####
+site_summary <- site_summary %>%
+  group_by(species_id) %>%
+  mutate(cwd.spstd = (cwd.ave - mean(cwd.ave)) / sd(cwd.ave),
+         aet.spstd = (aet.ave - mean(aet.ave)) / sd(aet.ave),
+         pet.spstd = (pet.ave - mean(pet.ave)) / sd(pet.ave))
+
+to_merge <- site_summary %>%
+  select("site_id", "species_id", "cwd.spstd", "aet.spstd", "pet.spstd")
+complete_df <- complete_df %>%
+  merge(y = to_merge, by = c("site_id", "species_id"), all.x = TRUE)
+
 #### Run first stage ####
+fit_mod <- function(d) {
+  felm(ring_width ~ cwd.an + pet.an + age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = d )
+}
+
 site_lm <- complete_df %>% 
   group_by(site_id, species_id) %>%
-  do(fit_site = felm(ring_width ~ cwd.an+age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = . ))
-  # do(fit_site = felm(ring_width ~ cwd.an +aet.an+age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = . ))
-  # do(fit_site = felm(ring_width ~ cwd.an +pet.an+age+I(age^2)+I(age^3)+I(age^4)|tree_id|0|0, data = . ))
+  nest() %>%
+  mutate(mod = map(data, fit_mod),
+         mod = map(mod, tidy)) %>%
+  unnest(mod) %>%
+  filter(term %in% c('cwd.an', 'pet.an'))
 
-siteCoef=tidy(site_lm[[3]][[1]])%>%filter(term=="cwd.an")
-for(i in 2:length(site_lm[[3]])){
-  siteCoef=rbind(siteCoef,tidy(site_lm[[3]][[i]])%>%filter(term=="cwd.an"))
-}
-siteCoef$site_id <- site_lm$site_id
-siteCoef$species_id <- site_lm$species_id
-siteCoef <- siteCoef %>%
-  left_join(site_summary, by = c("site_id","species_id"))
+siteCoef <- site_lm %>%
+  pivot_wider(names_from = "term", values_from = c("estimate", "std.error", "statistic", "p.value")) %>%
+  left_join(site_summary, by = c("site_id","species_id")) %>%
+  select(-data)
 
 #remove one super extreme outlier
-siteCoef_trimmed=siteCoef %>%
+siteCoef_trimmed <- siteCoef %>%
   group_by(species_id) %>%
-  mutate(qhigh=quantile(estimate,0.99,na.rm=T),
-         qlow=quantile(estimate,0.01,na.rm=T))
+  mutate(cwd.qhigh=quantile(estimate_cwd.an,0.99,na.rm=T),
+         cwd.qlow=quantile(estimate_cwd.an,0.01,na.rm=T),
+         pet.qhigh=quantile(estimate_pet.an,0.99,na.rm=T),
+         pet.qlow=quantile(estimate_pet.an,0.01,na.rm=T))
 siteCoef_trimmed=siteCoef_trimmed %>%
-  filter(estimate>qlow & estimate<qhigh)
+  filter(estimate_cwd.an>cwd.qlow & estimate_cwd.an<cwd.qhigh,
+         estimate_pet.an>pet.qlow & estimate_pet.an<pet.qhigh)
 
-### TEMP - KEEP ALL TO KEEP JUNIPER
-siteCoef_trimmed <- siteCoef
+
+### Explore first stage ####
+mod <- lm(estimate_cwd.an ~ cwd.spstd + pet.spstd, data= siteCoef)
+mod %>% summary()
+mod <- lm(estimate_pet.an ~ cwd.spstd + pet.spstd, data= siteCoef)
+mod %>% summary()
+
+siteCoef <- siteCoef %>%
+  group_by("species_id") %>%
+  mutate(cwd.q = ntile(cwd.ave, 5))
+p <- siteCoef_trimmed %>% ggplot(aes(group=cwd.q, y=estimate_cwd.an)) + 
+  geom_boxplot()
+p
+
+
+
+siteCoef_trimmed <- siteCoef_trimmed %>%
+  group_by("species_id") %>%
+  mutate(cwd.q = ntile(cwd.ave, 5),
+         pet.q = ntile(pet.ave, 5),
+         cwd.high = ntile(cwd.ave, 2)-1,
+         pet.high = ntile(pet.ave, 2)-1,
+         climate = paste0('cwd', cwd.high, '_pet', pet.high)) %>%
+  ungroup()
+siteCoef_trimmed <- siteCoef_trimmed %>%
+  mutate(cwd.q = as.factor(ntile(cwd.spstd, 7)),
+         pet.q = as.factor(ntile(pet.spstd, 7)),
+         cwd.high = as.factor(ntile(cwd.spstd, 2)-1),
+         pet.high = as.factor(ntile(pet.spstd, 2)-1),
+         climate = paste0('cwd', cwd.high, '_pet', pet.high))
+plotCoef <- siteCoef_trimmed %>%
+  filter(species_id == "pcgl") %>%
+    mutate(cwd.q = as.factor(ntile(cwd.spstd, 5)),
+         pet.q = as.factor(ntile(pet.spstd, 5)),
+         aet.q = as.factor(ntile(aet.spstd, 5)),
+         cwd.high = as.factor(ntile(cwd.spstd, 2)-1),
+         pet.high = as.factor(ntile(pet.spstd, 2)-1),
+         climate = paste0('cwd', cwd.high, '_pet', pet.high))
+
+p1 <- plotCoef %>% ggplot(aes(x=climate, y=estimate_cwd.an)) +
+  geom_boxplot()
+p2 <- plotCoef %>% ggplot(aes(x=climate, y=estimate_pet.an)) +
+  geom_boxplot()
+p1 / p2
+
+
+p1 <- plotCoef %>% ggplot(aes(x=cwd.q, y=estimate_cwd.an)) +
+  geom_boxplot()
+p2 <- plotCoef %>% ggplot(aes(x=cwd.q, y=estimate_pet.an)) +
+  geom_boxplot()
+p3 <- plotCoef %>% ggplot(aes(x=pet.q, y=estimate_cwd.an)) +
+  geom_boxplot()
+p4 <- plotCoef %>% ggplot(aes(x=pet.q, y=estimate_pet.an)) +
+  geom_boxplot()
+
+(p1 | p2) / (p3 | p4)
+
+
+p <- plotCoef %>% ggplot(aes(x=cwd.spstd, y=pet.spstd, colour = estimate_cwd.an)) +
+  geom_point() +
+  xlim(c(-2,2)) +
+  ylim(c(-2,2))
+p
+
+p <- plotCoef %>% ggplot(aes(x=cwd.spstd, y=pet.spstd, colour = estimate_pet.an)) +
+  geom_point() +
+  xlim(c(-2,2)) +
+  ylim(c(-2,2))
+p
+
+
+p <- plotCoef %>% ggplot(aes(x=aet.ave, y=cwd.ave, colour = estimate_cwd.an)) +
+  geom_point() 
+p
+
++ scale_colour_brewer(palette = "Greens")
+
+lm(estimate_cwd.an ~ cwd.spstd + I(cwd.spstd),weights=errorweights,data= .)
+
+
+
+
+# ### TEMP - KEEP ALL TO KEEP JUNIPER
+# siteCoef_trimmed <- siteCoef
 
 # Calculate limits for each species
 xlim.sp=site_summary %>%
@@ -125,9 +225,11 @@ xlim.sp=site_summary %>%
          aet.qlow = quantile(aet.ave, 0.05),
          aet.qmed = quantile(aet.ave,0.5)) %>%
   select(species_name,cwd.qhigh,cwd.qlow,cwd.qmed,pet.qhigh,pet.qlow,pet.qmed, aet.qhigh, aet.qlow, aet.qmed) %>%
-  distinct()
+  distinct() %>%
+  ungroup()
 
 xlim.all <- site_summary %>%
+  ungroup() %>%
   summarize(cwd.qhigh = quantile(cwd.ave,0.95),
             cwd.qlow = quantile(cwd.ave,0.05),
             cwd.qmed = quantile(cwd.ave,0.5),
@@ -142,9 +244,10 @@ xlim.all <- site_summary %>%
 # a=ggplot(siteCoef_trimmed,aes(x=cwd.ave,y=estimate,col=species))+theme_bw()+geom_point()+facet_wrap(~species)
 # a=a+labs(x="Average Climatic Water Deficit",y="Marginal Effect of CWD")+scale_color_discrete(guide=FALSE)
 
-kd <- kde2d(x = site_summary$cwd.ave, y = site_summary$pet.ave, n = 25, lims = c(c(0,750),c(100, 600)))
-points(site_summary$cwd.ave, site_summary$pet.ave)
+kd <- kde2d(x = site_summary$cwd.spstd, y = site_summary$pet.spstd, 
+            n = 25, lims = c(c(-2.5,2.5),c(-2.5, 2.5)))
 filled.contour(kd, color.palette = pal)
+# points(site_summary$cwd.spstd, site_summary$pet.spstd)
 
 x11()
 par(mfrow=c(4,5),mar=c(5,5,4,2))
@@ -163,9 +266,9 @@ for(i in 1:dim(xlim.sp)[1]){
 #### Run second stage of model  ####
 dat = siteCoef_trimmed %>% 
   ungroup() %>%
-  drop_na(std.error) %>%
-  mutate(errorweights=1/std.error/sum(1/std.error,na.rm=T)) 
-grandmodel <- lm(estimate~cwd.ave+I(cwd.ave^2) + I(cwd.ave^3) + pet.ave + I(pet.ave^2) + I(pet.ave^3),weights=errorweights,data= dat)
+  drop_na(std.error_cwd.an) %>%
+  mutate(errorweights=1/std.error_cwd.an/sum(1/std.error_cwd.an,na.rm=T)) 
+grandmodel <- lm(estimate_cwd.an~cwd.ave + I(cwd.ave^2),weights=errorweights,data= dat)
 # grandmodel <- lm(estimate~cwd.ave+I(cwd.ave^2), weights=errorweights,data= dat)
 
 cwd.x <- seq(xlim.all$cwd.qlow, xlim.all$cwd.qhigh, length.out=1000)
@@ -228,3 +331,28 @@ for(i in 1:dim(xlim.sp)[1]){
   abline(h=0,lty=2,lwd=2)
 }
 
+
+
+
+
+##### 
+#### Single-stage panel model #####
+low <- complete_df %>% filter(cwd.dev<=-0.1)
+mod_low <- felm(ring_width ~ cwd.an + I(cwd.an^2) + age + 
+                  I(age^2) + I(age^3) + I(age^4) | site_id + year, 
+                data = low)
+
+med <- complete_df %>% filter((cwd.dev<0.1) & (cwd.dev>-0.1))
+mod_med <- felm(ring_width ~ cwd.an + I(cwd.an^2) + age + 
+                  I(age^2) + I(age^3) + I(age^4) | tree_id + year, 
+                data = med)
+
+
+high <- complete_df %>% filter(cwd.dev>=0.1)
+mod_high <- felm(ring_width ~ cwd.an + I(cwd.an^2) + age + 
+                   I(age^2) + I(age^3) + I(age^4) | site_id + year, 
+                 data = high)
+
+mod <- felm(ring_width ~ cwd.an + aet.an + 
+              age + I(age^2) + I(age^3) + I(age^4) | tree_id + year, 
+            data = high)
