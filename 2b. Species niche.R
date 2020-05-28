3ff8a4a4f802adb6ca463f37a84da251addb9b14
rheilmayr <rheilmayr@gmail.com>
2020-05-28 07:28:17 -0700

    Moving figure generation

--- 2b. Species niche.R
+++ 2b. Species niche.R
@@ -10,7 +10,7 @@
 # - 
 #
 # Todo ideas:
-# - Could use both spatial and annual variation in CWD / AET to characterize niche
+# - Could use both spatial and annual variation in CWD / AET to characterize niche - Waiting on new data from Fran (5/25/20)
 #
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -21,8 +21,6 @@ library(tidyverse)
 library(dbplyr)
 library(RSQLite)
 library(ggplot2)
-library(rnaturalearth)
-library(rnaturalearthdata)
 library(sf)
 library(rgeos)
 library(stringr)
@@ -85,179 +83,5 @@ clim_df <- clim_df %>%
 write.csv(clim_df, paste0(wdir, "clim_niche.csv"))
 
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# CODE BELOW THIS POINT SHOULD BE MOVED TO DIFFERENT SCRIPT ------------------------------------
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Summarize ITRDB species frequencies ------------------------------------
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# 1. ITRDB data
-# obs_df <- read_delim(paste0(wdir,'gbif_pinopsida.csv'), delim = '\t')
-# obs_df$geom <- st_sfc(obs_df$decimalLatitude, obs_df$decimalLongitude)
-tree_db <- paste0(wdir, 'tree_ring_data_V2.db')
-sqlite <- dbDriver("SQLite")
-conn <- dbConnect(sqlite, tree_db)
-tables = dbListTables(conn)
-species_db = as.data.frame(tbl(conn, 'species'))
-tree_db = as.data.frame(tbl(conn, 'trees'))
-site_db = as.data.frame(tbl(conn, "sites"))
 
 
-
-spp_lookup <- read.csv(paste0(wdir, "itrdb_species_list.csv"))
-spp_lookup <- spp_lookup %>%
-  mutate(spp = paste0(genus, " ", species),
-         code = tolower(str_replace_all(code, "[*]", "")),
-         ncode = nchar(code)) %>%
-  arrange(spp) %>%
-  as_tibble()
-
-itrdb_species <- species_db %>%
-  arrange(species_id)
-
-site_count <- tree_db %>%
-  select(species_id, site_id) %>%
-  distinct() %>%
-  count(species_id) %>%
-  arrange(desc(n)) %>%
-  select(species_id, n_sites = n)
-
-site_count <- site_count %>%
-  left_join(spp_lookup, by = c("species_id" = "code"))
-
-tree_count <- tree_db %>%
-  select(species_id, site_id) %>%
-  count(species_id) %>%
-  arrange(desc(n)) %>%
-  select(species_id, n_trees = n)
-
-site_count <- site_count %>%
-  left_join(tree_count, by = "species_id")
-
-site_count <- site_count %>%
-  select(species_id, genus, species, spp, common_name, n_sites, n_trees)
-write.csv2(site_count, paste0(wdir, "species_summary.csv"))
-
-
-
-
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Map overlap between ITRDB and range ------------------------------------
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Define species
-spp_code <- 'psme'
-
-# Pull relevant ITRDB sites
-site_list <- tree_db %>%
-  filter(species_id == spp_code) %>% 
-  pull(site_id) %>% 
-  unique()
-
-sites <- site_db %>%
-  filter(site_id %in% site_list) %>%
-  select(site_id, latitude, longitude) %>%
-  drop_na() %>%
-  st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
-
-
-# Pull relevant range map
-sp_range <- range_sf %>% 
-  filter(sp_code == spp_code)
-
-
-xlims <- c(-132, -102)
-ylims <- c(26, 56)
-
-# Plot species ranges
-world <- ne_coastline(scale = "medium", returnclass = "sf")
-map <- ggplot() +
-  geom_sf(data = world) +
-  geom_sf(data = sp_range, fill = 'lightblue', alpha = .9) +
-  geom_sf(data = sites, color = 'darkblue', fill = 'red', alpha = .8) +
-  theme_bw(base_size = 22)+
-  ylab("Latitude")+
-  xlab("Longitude")+
-  coord_sf(xlim = xlims, ylim = ylims, expand = FALSE) ## Western US
-map
-
-
-library(raster)
-library(rgdal)
-library(viridis)
-
-
-cwd_historic_df <- cwd_historic %>% 
-  # mask(sp_range) %>%
-  as.data.frame(xy = TRUE) %>% 
-  drop_na() %>% 
-  filter(x>xlims[1], x<xlims[2], y>ylims[1], y<ylims[2]) %>% 
-  mutate(CWD = layer,
-         cwd.mean = mean(layer),
-         cwd.sd = sd(layer),
-         std_cwd = (layer - cwd.mean) / cwd.sd)
-map <- ggplot() +
-  geom_raster(data = cwd_historic_df, aes(x = x, y = y, fill = CWD)) +
-  # geom_sf(data = sp_range, fill = 'lightblue', alpha = .9) +
-  scale_fill_viridis_c() +
-  geom_sf(data = sites, color = 'red', alpha = 1) +
-  theme_bw(base_size = 22)+
-  ylab("Latitude")+
-  xlab("Longitude")+
-  geom_sf(data = world, color = 'white') +
-  coord_sf(xlim = xlims, ylim = ylims, expand = FALSE) ## Western US
-
-map
-
-flm_df <- read_csv(paste0(wdir, 'first_stage\\log_cwd_pet.csv')) %>%
-  select(-X1)
-
-sp_regs <- flm_df %>% 
-  filter(species_id == spp_code) %>% 
-  select(site_id, estimate_cwd.an, std.error_cwd.an, p.value_cwd.an)
-
-
-site_regs <- sites %>% 
-  left_join(sp_regs, by = c("site_id")) %>% 
-  mutate(ln_cwd = log(estimate_cwd.an)) %>% 
-  drop_na()
-  
-
-
-map <- ggplot() +
-  # geom_raster(data = cwd_historic_df, aes(x = x, y = y, fill = std_cwd)) +
-  # geom_sf(data = sp_range, fill = 'lightblue', alpha = .9) +
-  geom_sf(data = site_regs, aes(color = ln_cwd), alpha = 1) +
-  scale_fill_viridis_c() +
-  theme_bw(base_size = 22)+
-  ylab("Latitude")+
-  xlab("Longitude")+
-  geom_sf(data = world, color = 'white', size = 1) +
-  coord_sf(xlim = xlims, ylim = ylims, expand = FALSE) ## Western US
-
-map
-
-
-
-# coord_sf(crs = st_crs(54019), expand = FALSE)
-# coord_sf(xlim = c(-10, 60), ylim = c(30, 55), expand = FALSE) ## Europe
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Plot climate overlap of observations and species range -----------------
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-plot(cwd_historic)
-plot(pet_historic)
-plot(aet_historic)
-
-
-ggplot(data = aet_vals %>% as.data.frame(), aes(x=.)) +
-  geom_histogram()
-ggplot(data = cwd_vals %>% as.data.frame(), aes(x=.)) +
-  geom_histogram()
-
-hex <- clim_df %>% 
-  ggplot(aes(x = cwd, y = aet)) +
-  geom_hex()
-hex
