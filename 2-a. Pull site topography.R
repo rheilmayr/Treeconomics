0bc3b9cedfe3e75b3bd1b172a2164b923abe7331
rheilmayr <rheilmayr@gmail.com>
2020-07-10 17:27:31 -0700

    Noticed itrdb elevation was being exported

--- 2-a. Pull site topography.R
+++ 2-a. Pull site topography.R
@@ -23,6 +23,8 @@ wdir <- 'remote//'
 
 # 1. Load site latitude and longitude
 site <- read_csv(paste0(wdir, 'out//dendro//site_summary.csv'))
+site <- site %>% 
+  rename(elevation_itrdb = elevation)
 
 #2. Set Directory for DEM tiles
 demfiles=list.files("E:\\DEM Data",full.names=TRUE)
