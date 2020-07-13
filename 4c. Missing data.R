deceaa3ecce74b03828b31e0ce48075a1cd3e856
rheilmayr <rheilmayr@gmail.com>
2020-07-13 16:29:06 -0700

    Updating to new version of ITRDB data

--- 4c. Missing data.R
+++ 4c. Missing data.R
@@ -18,6 +18,7 @@ library(dbplyr)
 library(RSQLite)
 library(sf)
 library(naniar)
+library(stringr)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -26,28 +27,25 @@ library(naniar)
 # Define path
 wdir <- 'remote\\'
 
-# Load ITRDB database
-tree_db = paste0(wdir, 'tree_ring_data_V2.db')
-sqlite <- dbDriver("SQLite")
-conn <- dbConnect(sqlite, tree_db)
-tables = dbListTables(conn)
-tree_db = as.data.frame(tbl(conn, 'trees'))
-spp_db = as.data.frame(tbl(conn,'species'))
-site_db = as.data.frame(tbl(conn, "sites"))
-obs_db = tbl(conn, 'observations_new')
+# Load list of ITRDB sites
+itrdb_sites <- read_csv(paste0(wdir, 'out\\dendro\\site_summary.csv')) %>% 
+  select(collection_id, sp_id) %>% 
+  distinct() %>% 
+  mutate(sp_id = str_to_lower(sp_id)) %>% 
+  drop_na()
 
 # Load climate data
-cwd_csv = paste0(wdir, 'CRU//essentialcwd_data.csv')
+cwd_csv = paste0(wdir, 'out//climate//essentialcwd_data.csv')
 cwd_df <- read.csv(cwd_csv, sep=',')
 cwd_df <- cwd_df %>% 
-  mutate("site_id" = as.character(site)) 
+  mutate("collection_id" = as.character(site))
 cwd_sites <- cwd_df %>%
-  select(site_id) %>% 
+  select(collection_id) %>% 
   distinct() %>% 
   mutate(valid_clim = 1)
 
 # Load species niches
-range_file <- paste0(wdir, 'range_maps//merged_ranges.shp')
+range_file <- paste0(wdir, 'in//species_ranges//merged_ranges.shp')
 range_sf <- st_read(range_file)
 st_geometry(range_sf) = NULL
 range_species <- range_sf %>%
@@ -55,38 +53,42 @@ range_species <- range_sf %>%
   distinct() %>% 
   mutate(valid_range = 1)
 
-# Load list of valid dendrochronologies
-dendro_sites <- read_csv(paste0(wdir, 'out\\rwi_data\\2_valid_sites.csv')) %>% 
-  select(site_id) %>% 
+# Identify valid dendrochronologies
+dendro_df <- read_csv(paste0(wdir, 'out\\dendro\\rwi_long.csv')) %>% 
+  select(collection_id) %>% 
+  distinct() %>% 
   mutate(valid_dendro = 1)
 
 # Load first stage results
-first_stage <- read_csv(paste0(wdir, 'out\\first_stage\\', 'log_cwd_pet.csv'))
+first_stage <- read_csv(paste0(wdir, 'out\\first_stage\\tree_log_pet_cwd.csv'))
 first_stage <- first_stage %>% 
-  select(site_id, species_id) %>% 
+  select(collection_id) %>%
+  distinct() %>% 
   mutate(valid_fs = 1)
  
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Merge different datasets ------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Create list of valid species / site possibilities
-index <- tree_db %>%
-  select(species_id, site_id) %>% 
-  distinct()
-
 # Merge datasets to index
-data_inventory <- index %>% 
-  merge(cwd_sites, by = "site_id", all = TRUE) %>% 
-  merge(range_species, by = 'species_id', all = TRUE) %>% 
-  merge(dendro_sites, by = "site_id", all = TRUE) %>% 
-  merge(first_stage, by = c("site_id", "species_id"), all = TRUE)
+data_inventory <- itrdb_sites %>% 
+  merge(cwd_sites, by = "collection_id", all = TRUE) %>% 
+  merge(range_species, by.x = "sp_id", by.y = 'species_id', all = TRUE) %>% 
+  merge(dendro_df, by = "collection_id", all = TRUE) %>% 
+  merge(first_stage, by = "collection_id", all = TRUE)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Explore possible data problems ------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-dim(index)
+dim(itrdb_sites)
 data_inventory %>% gg_miss_upset()
+miss_var_summary(data_inventory)
 
 
+
+#### NOTES
+# Missing dendro: Largely due to strange format of ITRDB files, leading to parsing errors
+# Missing first stage: For ones with valid dendro, missing due to no variation in CWD / PET
+# Missing range: A few species dominate combined with sites that don't specify beyond genera
+# Climate: Should dig in to this again, think I am missing some data here
 
