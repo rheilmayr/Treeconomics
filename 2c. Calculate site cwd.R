76f53bc75ccf6bbe2f315f49bf7306761e7a5042
rheilmayr <rheilmayr@gmail.com>
2022-07-07 09:31:46 -0700

    Running missing data exploration after cwd calculations

--- 2c. Calculate site cwd.R
+++ 2c. Calculate site cwd.R
@@ -51,20 +51,8 @@ data$tmean=(data$tmn_corrected+data$tmx_corrected)/2
 data$slope <- data$slope * 57.2958 # convert slope from radians to degrees
 data$aspect <- data$aspect * 57.2958 # convert aspect from radians to degrees
 
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Characterize missing data ----------------------------------------------
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data <- data %>% 
   select(site_id, slope, latitude, longitude, aspect, pre_corrected, tmean, month, year, swc)
-
-data %>% gg_miss_upset()
-any_missing <- data %>%
-  filter_all(any_vars(is.na(.)))
-share_missing <- dim(any_missing)[1] / dim(data)[1]
-share_missing
-miss_var_summary(data)
-
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Calculate CWD and save data --------------------------------------------
@@ -78,6 +66,37 @@ cwd_data<-cwd_function(site=data$site_id,slope=data$slope,latitude=data$latitude
                        tmean=data$tmean,month=data$month,year=data$year,
                        soilawc=data$swc,type="annual")
 # fwrite(cwd_data,file=paste0(wdir,"out/climate/cwd_data_200620.csv"))
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Characterize missing data ----------------------------------------------
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+input_data <- data %>% 
+  select(site_id, slope, latitude, longitude, aspect, pre_corrected, tmean, month, year, swc)
+
+input_data %>% gg_miss_upset()
+any_missing <- input_data %>%
+  filter_all(any_vars(is.na(.)))
+share_missing <- dim(any_missing)[1] / dim(data)[1]
+share_missing
+miss_var_summary(data)
+
+
+out_data <- cwd_data %>% 
+  select(site, year, month, wm, petm, soilm, deltsoil, aet, cwd)
+
+out_data %>% gg_miss_upset()
+any_missing <- input_data %>%
+  filter_all(any_vars(is.na(.)))
+share_missing <- dim(any_missing)[1] / dim(data)[1]
+share_missing
+miss_var_summary(data)
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Write out file ----------------------------------------------
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 cwd_data <- cwd_data %>%
   select(site, year, month, aet, cwd)
 fwrite(cwd_data,file=paste0(wdir,"out/climate/essentialcwd_data.csv"))
