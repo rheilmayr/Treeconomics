1ab0b926ccf4ab464229181560ca3bc19dcd4773
rheilmayr <rheilmayr@gmail.com>
2022-09-05 14:34:52 -0700

    Building in stats for results sections of paper

--- 7c. Paper stats.R
+++ 7c. Paper stats.R
@@ -80,11 +80,17 @@ sp_predictions <- do.call('rbind', lapply(rwi_list, readRDS))
 
 
 # 6. Second stage model
-mod_df <- trim_df
-cwd_mod <- readRDS(paste0(wdir, "out/second_stage/cwd_mod.rds"))
-cwd_vcov <- readRDS(paste0(wdir, "out/second_stage/cwd_mod_vcov.rds"))
-pet_mod <- readRDS(paste0(wdir, "out/second_stage/pet_mod.rds"))
-int_mod <- readRDS(paste0(wdir, "out/second_stage/int_mod.rds"))
+ss_df <- read_rds(paste0(wdir, "out/second_stage/ss_bootstrap.rds"))
+
+# mod_df <- trim_df
+# cwd_mod <- readRDS(paste0(wdir, "out/second_stage/cwd_mod.rds"))
+# cwd_vcov <- readRDS(paste0(wdir, "out/second_stage/cwd_mod_vcov.rds"))
+# pet_mod <- readRDS(paste0(wdir, "out/second_stage/pet_mod.rds"))
+# int_mod <- readRDS(paste0(wdir, "out/second_stage/int_mod.rds"))
+
+constant_sensitivities <- 
+  read_rds(paste0(wdir, "out/first_stage/constant_sensitivities.rds"))
+
 
 # 7. Genus model predictions
 genus_predictions <- readRDS(paste0(wdir, "out/second_stage/genus_mods.rds"))
@@ -101,33 +107,46 @@ names(cwd_historic) = "cwd"
 # Abstract and intro --------------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ## Summary stats of data included in analysis
-(n_species <- flm_df %>% 
+
+## Number of species
+n_species <- flm_df %>% 
   pull(species_id) %>% 
   unique() %>% 
-  length())
+  length() %>% 
+  print()
 
-(n_sites <- flm_df %>% 
+## Number of sites
+n_sites <- flm_df %>% 
   pull(collection_id) %>% 
   unique() %>% 
-  length())
+  length() %>% 
+  print()
 
-(n_trees <- flm_df %>% 
+## Number of trees
+n_trees <- flm_df %>% 
   pull(ntrees) %>% 
-  sum())
+  sum() %>% 
+  print()
 
-(n_obs <- flm_df %>% 
+## Number of tree-year observations
+n_obs <- flm_df %>% 
   pull(nobs) %>% 
-  sum(na.rm = TRUE))
-### TODO: Some rows have nobs = NA but have estimated sensitivity; need to go back to original processing to resolve this 
+  sum() %>% 
+  print()
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Impacts of water and energy on tree growth  ----------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ## In the median plot, a one standard deviation increase in CWD was associated with an XX percent decline in annual growth"
-(cwd_te_median <- flm_df %>%
-  pull(estimate_cwd.an) %>% 
-  median())
+# cwd_te_median <- flm_df %>%
+#   pull(estimate_cwd.an) %>% 
+#   median() %>% 
+#   print()
+constant_sensitivities$cwd$estimate[1]
+constant_sensitivities$cwd$ci_dif[1]
+
+
 
 ## The site-level, marginal effect of CWD was significantly (p<0.05) negative for XX percent of sites, while only XX 
 ## percent of sites exhibited a significantly positive relationship. 
@@ -150,10 +169,15 @@ cwd_te_count_pos <- flm_df %>%
 (cwd_te_shr_pos <- cwd_te_count_pos / n_sites)
 
 
-##  In contrast, a one standard deviation increase in PET was, on average, associated with an XX percent increase in growth. 
-(cwd_te_median <- flm_df %>%
-    pull(estimate_pet.an) %>% 
-    median())
+##  In contrast, a one standard deviation increase in PET was, on average, associated with an XX percent increase in growth.
+# pet_te_median <- flm_df %>%
+#   pull(estimate_pet.an) %>%
+#   median() %>%
+#   print()
+constant_sensitivities$pet$estimate[1]
+constant_sensitivities$pet$ci_dif[1]
+
+
 
 ## The relationship between PET and growth was significantly positive for XX percent of sites, and negative for XX percent of sites.
 pet_te_count_neg <- flm_df %>%
@@ -178,5 +202,69 @@ pet_te_count_pos <- flm_df %>%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Relatively dry sites exhibit less sensitivity to drought  --------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# For example, plots sites with a historic, mean CWD exceeding their species’ 
+# average historic CWD value experienced across a species’ range experienced an
+# 3.3+/-3.7XX percent percent decline in growth in years with in response to a
+# one standard deviation increase in CWD (Figure 3, Panel A). 
+mod_df <- trim_df %>% 
+  filter(cwd.spstd > 0)
+cwd_high_mod <- feols(estimate_cwd.an ~ 1,
+                       weights = mod_df$cwd_errorweights, data = mod_df,
+                       vcov = conley(cutoff = 363, distance = "spherical")) %>% 
+  tidy(conf.int = TRUE, conf.level = 0.95) %>% 
+  mutate(ci_dif = estimate - conf.low) %>% 
+  print()
 
+# In contrast, sites with a lower than average historic CWD (i.e., relatively cooler 
+# and wetter regions of a species’ niche) experienced an 8.9+/-3.9 percent decline 
+# in growth from a one standard deviation increase in CWD.
+mod_df <- trim_df %>% 
+  filter(cwd.spstd < 0)
+cwd_low_mod <- feols(estimate_cwd.an ~ 1,
+                       weights = mod_df$cwd_errorweights, data = mod_df,
+                       vcov = conley(cutoff = 363, distance = "spherical")) %>% 
+  tidy(conf.int = TRUE, conf.level = 0.95) %>% 
+  mutate(ci_dif = estimate - conf.low) %>% 
+  print()
+
+# After controlling for the correlation between historic CWD and PET, we estimate that 
+# the slope of the line describing the relationship between historic CWD and sensitivity 
+# to CWD is XX (Figure 2B). In other words, while a one standard deviation shock to CWD 
+# is anticipated to lead to an XX% decline in growth in sites located at a species’ mean 
+# CWD, a comparable shock to CWD would lead to only an XX% decline in growth for sites 
+# that are historically one standard deviation dryer than the species mean.
+ss_df %>%
+  summarise(mean_cwd = mean(cwd_cwd),
+            lower_cwd = quantile(cwd_cwd, 0.025),
+            upper_cwd = quantile(cwd_cwd, 0.975))
+
+
+
+
+pull_marg_fx <- function(at_pet, at_cwd, mod_df){
+  cwd_me_predictions <- mod_df$cwd_int + (at_cwd * mod_df$cwd_cwd) + (at_pet * mod_df$cwd_pet)
+  cwd_ci_min <- cwd_me_predictions %>% quantile(0.025)
+  cwd_ci_max <- cwd_me_predictions %>% quantile(0.975)
+  cwd_mean <- cwd_me_predictions %>% mean()
+  
+  pet_me_predictions <- mod_df$pet_int + (at_cwd * mod_df$pet_cwd) + (at_pet * mod_df$pet_pet)
+  pet_ci_min <- pet_me_predictions %>% quantile(0.025)
+  pet_ci_max <- pet_me_predictions %>% quantile(0.975)
+  pet_mean <- pet_me_predictions %>% mean()
+  return(tibble(cwd_mean = cwd_mean, cwd_ci_min = cwd_ci_min, cwd_ci_max = cwd_ci_max,
+                pet_mean = pet_mean, pet_ci_min = pet_ci_min, pet_ci_max = pet_ci_max))
+}
+
+
+at_pet <- 0
+init_pull_marg_fx = partial(.f = pull_marg_fx, mod_df = ss_df)
+cwd_me_df <- tibble(at_cwd = c(0, 1))
+cwd_me_df <- cwd_me_df %>%
+  mutate(cwd_me = pmap(list(at_pet = at_pet,
+                            at_cwd = cwd_me_df$at_cwd),
+                       .f = init_pull_marg_fx)) %>% 
+  unnest(cwd_me) %>% 
+  mutate(cwd_ci_dif = cwd_mean - cwd_ci_min,
+         cwd_ci_dif2 = cwd_ci_max - cwd_mean) %>% 
+  print()
 
