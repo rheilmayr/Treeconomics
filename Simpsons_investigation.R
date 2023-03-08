f440a1cb842ef6600ba942a4ddc2f8f566918ff3
Joan Dudney <jdudney@berkeley.edu>
2023-03-08 11:22:52 -0800

    species-level analysis of simpson's

--- Simpsons_investigation.R
+++ Simpsons_investigation.R
@@ -30,19 +30,18 @@ library(sf)
 library(gstat)
 library(units)
 
-
-
+select <- dplyr::select
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Load data --------------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Define path
-wdir <- 'remote/'
+wdir <- 'NewRemote/'
 
 # 1. Site-level regressions
-flm_df <- read_csv(paste0(wdir, 'out/first_stage/site_pet_cwd_std.csv')) %>%
-  select(-X1)
+flm_df <- read_csv(paste0(wdir, 'out/first_stage/site_pet_cwd_std.csv')) 
+  #select(-X1)
 
 # 2. Historic site-level climate
 ave_site_clim <- read_rds(paste0(wdir, "out/climate/site_ave_clim.gz"))
@@ -75,6 +74,7 @@ flm_df <- flm_df %>%
            (estimate_cwd.an>cwd_est_bounds[2]) |
            (estimate_pet.an<pet_est_bounds[1]) |
            (estimate_pet.an>pet_est_bounds[2]))
+
 trim_df <- flm_df %>% 
   filter(outlier==0) %>% 
   drop_na()
@@ -83,35 +83,189 @@ trim_df <- flm_df %>%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Plot sensitivity --------------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+theme_set(
+  theme_bw(base_size = 15)+
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),text=element_text(family ="Helvetica"))
+)
+
+
+##=================================================================================================================
+##             PONDEROSA PINE         
+##==================================================================================================================
+
+# plot_df <- flm_df %>%
+#   filter(species_id == "pipo")
+# 
+# allpipo=plot_df %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marginal effect of CWD", x="Historic CWD (SD)")+
+#   ggtitle("Pinus ponderosa (PIPO)")
+# 
+# unique(plot_df$collection_id)
+# 
+# ## Look at specific site and its nearby sites
+# site_id <- "AZ036"
+# site_id <- "NM512"
+# site_id <- "WY058"
+# 
+# proximate_ids <- block_list[site_id] %>% unlist(use.names = FALSE)
+# 
+# plot_df_small <- plot_df %>%
+#   filter(collection_id %in% proximate_ids)
+# 
+# azpipo <- plot_df_small %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+#   ggtitle(paste("PIPO: < 400km of", site_id))
+# 
+# azpipo
+# 
+# nmpipo <- plot_df_small %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+#   ggtitle(paste("PIPO: < 400km of", site_id))
+# 
+# nmpipo
+# 
+# wypipo <- plot_df_small %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+#   ggtitle(paste("PIPO: < 400km of", site_id))
+# 
+# wypipo
+# 
+# pipolots <- allpipo | (azpipo/nmpipo/wypipo)
+# pipolots + plot_annotation(tag_levels="A") & theme(
+#                 plot.tag = element_text(face = 'bold', size=15, family ="Helvetica"),
+#                 text=element_text(family ="Helvetica"))
+
+
+# ##=================================================================================================================
+# ##                PSME      
+# ##==================================================================================================================
+# plot_df <- flm_df %>%
+#   filter(species_id == "psme")
+# 
+# allpsme=plot_df %>%
+#   filter(estimate_cwd.an<1) %>% 
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marginal effect of CWD", x="Historic CWD (SD)")+
+#   ggtitle("Pseudotsuga menziesii (psme)")
+# 
+# allpsme
+# unique(plot_df$collection_id)
+# 
+# ## Look at specific site and its nearby sites
+# site_id <- "AZ558"
+# site_id <- "MT154"
+# site_id <- "CO052"
+# 
+# proximate_ids <- block_list[site_id] %>% unlist(use.names = FALSE)
+# 
+# plot_df_small <- plot_df %>%
+#   filter(collection_id %in% proximate_ids)
+# 
+# azpsme <- plot_df_small %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+#   ggtitle(paste("PSME: < 400km of", site_id))
+# 
+# azpsme
+# 
+# mtpsme <- plot_df_small %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+#   ggtitle(paste("PSME: < 400km of", site_id))
+# 
+# mtpsme
+# 
+# copsme <- plot_df_small %>%
+#   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+#   geom_point()+
+#   geom_smooth(method="loess", span=1)+
+#   labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+#   ggtitle(paste("PSME: < 400km of", site_id))
+# 
+# copsme
+# 
+# psmelots <- allpsme | (azpsme/mtpsme/copsme)
+# psmelots + plot_annotation(tag_levels="A") & theme(
+#   plot.tag = element_text(face = 'bold', size=15, family ="Helvetica"),
+#   text=element_text(family ="Helvetica"))
+
+##=================================================================================================================
+##                PIED     
+##==================================================================================================================
 plot_df <- flm_df %>%
-  filter(species_id == "pipo")
+  filter(species_id == "pied")
 
-
-plot_df %>%
+allpied=plot_df %>%
+  filter(estimate_cwd.an<1) %>% 
   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
-  geom_point()
+  geom_point()+
+  geom_smooth(method="loess", span=1)+
+  labs(y="Marginal effect of CWD", x="Historic CWD (SD)")+
+  ggtitle("Pinus edulis (PIED)")
+
+allpied
+unique(plot_df$collection_id)
 
 ## Look at specific site and its nearby sites
-site_id <- "AZ036"
-site_id <- "NM512"
-site_id <- "CA525"
+site_id <- "AZ102"
+site_id <- "NM038"
+site_id <- "UT534"
 
 proximate_ids <- block_list[site_id] %>% unlist(use.names = FALSE)
 
-plot_df <- plot_df %>%
+plot_df_small <- plot_df %>%
   filter(collection_id %in% proximate_ids)
 
-plot_df %>%
+azpied <- plot_df_small %>%
   ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
-  geom_point()
+  geom_point()+
+  geom_smooth(method="loess", span=1)+
+  labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+  ggtitle(paste("PIED: < 400km of", site_id))
 
+azpied
 
+nmpied <- plot_df_small %>%
+  ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+  geom_point()+
+  geom_smooth(method="loess", span=1)+
+  labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+  ggtitle(paste("PIED: < 400km of", site_id))
 
+nmpied
 
+utpied <- plot_df_small %>%
+  ggplot(aes(y = estimate_cwd.an, x = cwd.spstd)) +
+  geom_point()+
+  geom_smooth(method="loess", span=1)+
+  labs(y="Marg. effect CWD", x="Historic CWD (SD)")+
+  ggtitle(paste("PIED: < 400km of", site_id))
 
+utpied
 
-
-
+piedlots <- allpied | (azpied/nmpied/utpied)
+piedlots + plot_annotation(tag_levels="A") & 
+  theme(plot.tag = element_text(face = 'bold', size=15, family ="Helvetica"),
+  text=element_text(family ="Helvetica"))
 
 
 
@@ -150,7 +304,7 @@ for (site in site_list){
     pull(collection_id)
   block_list[site] <- list(block_sites)
 }
-save(block_list,file=paste0(wdir,"out/spatial_blocks_", as.character(threshold/1000), ".Rdat"))
+#save(block_list,file=paste0(wdir,"out/spatial_blocks_", as.character(threshold/1000), ".Rdat"))
 load(file=paste0(wdir,"out/spatial_blocks.Rdat"))
 
 
@@ -158,7 +312,7 @@ load(file=paste0(wdir,"out/spatial_blocks.Rdat"))
 # Simulate regional studies --------------------------------------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 rand_order <- flm_df %>%
-  filter(species_id =="pipo") %>%
+  #filter(species_id =="pied") %>%
   pull(collection_id) %>%
   sample()
 
@@ -225,7 +379,7 @@ block_df <- block_df %>%
 block_df <- block_df %>% 
   # filter(p.value < 0.05) %>%
   mutate(sig = p.value < 0.05,
-         effect = ifelse(sig & (estimate > 0), "spoiled", ifelse(sig & (estimate < 0), "range-edge", "neither")),
+         effect = ifelse(sig & (estimate > 0), "Drought-naive", ifelse(sig & (estimate < 0), "range-edge", "neither")),
          spoiled = estimate > 0)
 
 block_df %>%
@@ -236,18 +390,27 @@ block_df %>%
 block_df %>%
   mutate(sig = p.value < 0.05) %>%
   filter(estimate >-10, estimate < 10) %>%
-  ggplot(aes(x = pet.spstd, y = estimate)) +
+  ggplot(aes(x = cwd.spstd, y = estimate, color=effect, label=collection_id)) +
   geom_point() +
-  # geom_smooth() +
   theme_bw() +
-  geom_hline(yintercept = 0)
+  geom_hline(yintercept = 0, linetype="dashed")+
+  geom_text(hjust=0, vjust=0)
+
+block_df %>%
+  mutate(sig = p.value < 0.05) %>%
+  filter(estimate >-10, estimate < 10) %>%
+  ggplot(aes(x = cwd.spstd, y = estimate, color=effect)) +
+  geom_point() +
+  theme_bw() +
+  geom_hline(yintercept = 0, linetype="dashed")
+
 
 block_df %>%
   filter(pet.spstd > -0.25, estimate < -0.5)
 
-lasi pisi
+
 mod_df <- flm_df %>% 
-  filter(species_id == "pisi") 
+  filter(species_id == "pied") 
 
 mod <- lm(estimate_cwd.an ~ pet.spstd + cwd.spstd, data = mod_df)
 summary(mod)
