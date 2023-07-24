2e08cd736cdfa36e576e7453bbafdefd64ecb3ae
rheilmayr <rheilmayr@ucsb.edu>
2023-07-24 16:42:38 -0700

    Exploring single stage RE model

--- 8 Review comment 3_3.R
+++ 8 Review comment 3_3.R
@@ -36,6 +36,7 @@ library(marginaleffects)
 library(prediction)
 library(margins)
 library(lme4)
+library(multcomp)
 
 
 
@@ -93,12 +94,135 @@ dendro_df <- dendro_df %>%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Run model ---------------------------
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-mod <- feols(rwi ~ cwd.an.spstd + cwd.an.spstd:pet.spstd + cwd.an.spstd:pet.spstd**2 + cwd.an.spstd:cwd.spstd + cwd.an.spstd:cwd.spstd**2 +
-               pet.an.spstd + pet.an.spstd:pet.spstd + pet.an.spstd:pet.spstd**2 + pet.an.spstd:cwd.spstd + pet.an.spstd:cwd.spstd**2 | collection_id + species_id + tree_id, data = dendro_df)
-# mod <- lmer(rwi ~ cwd.an.spstd + cwd.an.spstd:pet.spstd + cwd.an.spstd:pet.spstd**2 + cwd.an.spstd:cwd.spstd + cwd.an.spstd:cwd.spstd**2 +
-#                pet.an.spstd + pet.an.spstd:pet.spstd + pet.an.spstd:pet.spstd**2 + pet.an.spstd:cwd.spstd + pet.an.spstd:cwd.spstd**2 + (1 | collection_id), data = dendro_df)
+ss_df <- read_csv(paste0(wdir, "out/first_stage/site_pet_cwd_std_augmented.csv"))
 
+mod_re <- lmer(rwi ~ cwd.an.spstd:pet.spstd + cwd.an.spstd:I(pet.spstd**2) + 
+              cwd.an.spstd:cwd.spstd + cwd.an.spstd:I(cwd.spstd**2) +
+              pet.an.spstd:pet.spstd + pet.an.spstd:I(pet.spstd**2) + 
+              pet.an.spstd:cwd.spstd + pet.an.spstd:I(cwd.spstd**2) + 
+              (1 + cwd.an.spstd + pet.an.spstd | collection_id), data = dendro_df)
+summary(mod_re)
+
+
+cwd_median <- ss_df %>% dplyr::select(cwd.spstd) %>% drop_na() %>% pull(cwd.spstd) %>% median()
+test_str <- paste0("cwd.an.spstd:cwd.spstd + (2 * cwd.an.spstd:I(cwd.spstd^2) * ", as.character(cwd_median), ") = 0")
+# test_str <- paste0("cwd.an.spstd:cwd.spstd = 0")
+lincom <- glht(mod_re, linfct = c(test_str))
+lincom <- summary(lincom)
+coef <- lincom$test$coefficients
+se <- lincom$test$sigma
+
+confint(lincom)
+
+
+
+
+## Contrast to primary specification confidence interval
+ss_bs <- read_rds(paste0(wdir, "out/second_stage/ss_bootstrap.rds"))
+ss_bs %>% pull(cwd_cwd) %>% quantile(c(0.025, 0.975))
+
+
+
+mod_fe <- feols(rwi ~ cwd.an.spstd:cwd.spstd | 
+                  collection_id, data = dendro_df)
+
+
+
+
+# mod_df <- dendro_df %>% 
+#   sample_n(100000)
+# 
+# ## FE model
+# mod_fe <- feols(rwi ~ cwd.an.spstd + cwd.an.spstd:pet.spstd + cwd.an.spstd:pet.spstd**2 + 
+#                cwd.an.spstd:cwd.spstd + cwd.an.spstd:cwd.spstd**2 +
+#                pet.an.spstd + pet.an.spstd:pet.spstd + pet.an.spstd:pet.spstd**2 + 
+#                pet.an.spstd:cwd.spstd + pet.an.spstd:cwd.spstd**2 +
+#                cwd.an.spstd * collection_id + pet.an.spstd * collection_id | 
+#                collection_id, data = mod_df)
+# summary(mod_fe)
+
+ss_df <- read_csv(paste0(wdir, "out/first_stage/site_pet_cwd_std_augmented.csv"))
+ss_modifications <- ss_df %>% 
+  select(collection_id, outlier, cwd_errorweights)
+
+dendro_df <- dendro_df %>% 
+  left_join(ss_modifications, by = "collection_id")
+
+## FE model - recreate primary results
+mod_df <- dendro_df %>% 
+  filter(outlier == 0)
+
+site_counts <- mod_df %>% 
+  group_by(collection_id) %>% 
+  tally()
+
+mod_df <- mod_df %>% 
+  left_join(site_counts, by = "collection_id") %>% 
+  mutate(weights = 1 / n)
+
+mod <- feols(rwi ~ cwd.an.spstd + cwd.an.spstd:pet.spstd + cwd.an.spstd:pet.spstd**2 + 
+               cwd.an.spstd:cwd.spstd + cwd.an.spstd:cwd.spstd**2 +
+               pet.an.spstd + pet.an.spstd:pet.spstd + pet.an.spstd:pet.spstd**2 + 
+               pet.an.spstd:cwd.spstd + pet.an.spstd:cwd.spstd**2 | 
+               collection_id, data = mod_df)
 summary(mod)
+
+mod <- feols(rwi ~ cwd.an.spstd + cwd.an.spstd:pet.spstd + 
+               cwd.an.spstd:cwd.spstd +
+               pet.an.spstd + pet.an.spstd:pet.spstd + 
+               pet.an.spstd:cwd.spstd | 
+               collection_id, data = mod_df)
+summary(mod)
+
+mod <- feols(rwi ~ cwd.an.spstd + pet.an.spstd | collection_id, 
+             data = mod_df)
+summary(mod)
+ss_df %>% filter(outlier==0) %>% select(estimate_pet.an, estimate_cwd.an) %>%  summary()
+
+ss_mod <- lm(estimate_cwd.an ~ cwd.spstd + pet.spstd, data = ss_df %>% filter(outlier == 0))
+summary(ss_mod)
+
+## RE model
+mod <- lmer(rwi ~ cwd.an.spstd + cwd.an.spstd:pet.spstd + cwd.an.spstd:I(pet.spstd**2) + 
+              cwd.an.spstd:cwd.spstd + cwd.an.spstd:I(cwd.spstd**2) +
+               pet.an.spstd + pet.an.spstd:pet.spstd + pet.an.spstd:I(pet.spstd**2) + 
+              pet.an.spstd:cwd.spstd + pet.an.spstd:I(cwd.spstd**2) + 
+              (1 | collection_id), data = dendro_df)
+summary(mod)
+
+
+
+
+
+mod_slopes <- slopes(mod, newdata = datagrid(cwd.an.spstd = 1, pet.an.spstd = 0, 
+                                             pet.spstd = 0, cwd.spstd = cwd_median))
+
+
+
+slope_plot <- plot_slopes(mod, variables = "cwd.an.spstd", condition = "cwd.spstd") 
+slope_plot +
+  xlim(-2.5, 2.5) +
+  theme_bw() +
+  ylim(-0.25, 0)
+
+slope_plot <- plot_slopes(mod, variables = "cwd.an.spstd", condition = "pet.spstd") 
+slope_plot +
+  xlim(-2.5, 2.5) +
+  theme_bw() +
+  ylim(-0.25, 0)
+
+slope_plot <- plot_slopes(mod, variables = "pet.an.spstd", condition = "cwd.spstd") 
+slope_plot +
+  xlim(-2.5, 2.5) +
+  theme_bw() +
+  ylim(0.4, 0.8)
+
+slope_plot <- plot_slopes(mod, variables = "pet.an.spstd", condition = "pet.spstd") 
+slope_plot +
+  xlim(-2.5, 2.5) +
+  theme_bw() +
+  ylim(0.5, 0.75)
+
 
 
 marg_fx_df <- function(mod){
@@ -115,7 +239,6 @@ cwd_pred_0 <- predictions(mod, newdata = datagrid(cwd.an.spstd = 0, pet.an.spstd
   mutate(variation = "cwd")
 cwd_pred_1 <- predictions(mod, newdata = datagrid(cwd.an.spstd = 1, pet.an.spstd = 0, pet.spstd = 0, cwd.spstd = seq(min,max,inc)))
 
-preds <- marg_fx_df(mod)
 adjustment <- cwd_pred_0$estimate[1]
 cwd_pred_1$adj_estimate <- cwd_pred_1$estimate - adjustment
 cwd_pred_1$adj_conf.high <- cwd_pred_1$conf.high - adjustment
