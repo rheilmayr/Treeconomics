e287fb45f16df04bd3fb503c712d7cd20d7e5656
rheilmayr <rheilmayr@ucsb.edu>
2023-08-16 10:12:39 -0700

    Expanding explorations

--- Species_level_investigation.R
+++ Species_level_investigation.R
@@ -25,7 +25,7 @@ sp_reg <- function(sp_data){
 sp_nest <- trim_df %>% 
   group_by(species_id) %>% 
   mutate(species_n = n()) %>%
-  filter(species_n > 50) %>% 
+  filter(species_n > 20) %>% 
   nest()
 
 sp_regs <- sp_nest %>% 
@@ -42,8 +42,151 @@ sp_regs %>%
   filter(p.value > 0.05)
 
 
+
 mod <- lm(estimate ~ pet_mean + cwd_mean, data = sp_regs)
+summary(mod)
+
+mod <- lm(estimate ~ pet_mean + cwd_mean, data = sp_regs %>% filter(p.value <=0.05))
+summary(mod)
+
+mod <- lm(estimate ~ pet_mean + cwd_mean, data = sp_regs, weights = (1/std.error))
 summary(mod)
 
 mod <- lm(estimate ~ pet_sd + cwd_sd, data = sp_regs)
 summary(mod)
+
+mod <- lm(estimate ~ pet_mean + pet_sd + cwd_mean + cwd_sd, data = sp_regs)
+summary(mod)
+
+### Genus
+trim_df <- fs_spl %>% 
+  filter(outlier==0) %>% 
+  drop_na()
+
+run_ss_conley <- function(data, outcome = "cwd_coef"){
+  if (outcome == "cwd_coef") {
+    error_weights = data$cwd_errorweights
+  } else if (outcome == "pet_coef") {
+    error_weights = data$pet_errorweights
+  } else if (outcome == "int_coef") {
+    error_weights = data$int_errorweights
+  }
+  formula <- as.formula(paste(outcome, " ~ cwd.spstd + (cwd.spstd^2) + pet.spstd + (pet.spstd^2)"))
+  mod <- feols(formula, data=data, weights = error_weights,
+               vcov = conley(cutoff = vg.range/1000, distance = "spherical"))
+  
+  # formula <- as.formula(paste(outcome, " ~ cwd.spstd + I(cwd.spstd**2) + pet.spstd + I(pet.spstd**2)"))
+  # mod <- lm(formula, data=data, weights = error_weights)
+  return(mod)
+}
+
+# run_ss_conley <- function(data, outcome = "estimate_cwd.an"){
+#   formula <- as.formula(paste(outcome, " ~ cwd.spstd + pet.spstd")) ## ADD BACK WEIGHTING HERE?
+#   mod <- feols(formula, data = data, weights = data$cwd_errorweights, 
+#         vcov = conley(cutoff = vg.range/1000, distance = "spherical"))
+#   return(mod)
+# }
+
+genus_lookup <- trim_df %>% select(collection_id, genus)
+genus_freq <- trim_df %>% 
+  group_by(genus) %>% 
+  summarise(n_collections = n_distinct(collection_id),
+            min_cwd = min(cwd.spstd),
+            max_cwd = max(cwd.spstd),
+            range_cwd = max_cwd - min_cwd) %>% 
+  arrange(desc(n_collections))
+
+gymno_key <- sp_info %>% 
+  select(genus, gymno_angio) %>% 
+  unique()
+
+
+
+# N = 10: 16 genera; 26: 12 genera; 50: 9 genera
+genus_keep <- genus_freq %>%
+  top_n(12,n_collections) %>% 
+  pull(genus)
+
+
+genus_df <- trim_df %>% 
+  mutate(int_coef = estimate_intercept,
+         pet_coef = estimate_pet.an, 
+         cwd_coef = estimate_cwd.an) %>% 
+  filter(genus %in% genus_keep) %>% 
+  group_by(genus) %>% 
+  nest() %>% 
+  mutate(model_estimates = map(data, run_ss_conley))
+
+genus_df$model_estimates
+
+
+
+### Binned plot of cwd sensitivity
+gen_marg_fx_df <- function(gen_mod, gen_data){
+  # cwd_min = 
+  cwd_inc <- 0.1
+  at_pet <- 0
+  cwd_min <- gen_data %>% pull(cwd.spstd) %>% min()
+  cwd_max <- gen_data %>% pull(cwd.spstd) %>% max()
+  gen_pred <- predictions(gen_mod, newdata = datagrid(pet.spstd = 0, cwd.spstd = seq(cwd_min,cwd_max,cwd_inc)))
+  return(gen_pred)
+}
+
+
+pull_coefs <- function(gen_mod, gen_dat){
+  median_cwd <- gen_dat %>% pull(cwd.spstd) %>% median()
+  lht <- linearHypothesis(gen_mod, c(paste0('cwd.spstd + ', as.character(median_cwd), ' * I(cwd.spstd^2) = 0')))
+  pvalue <- lht$`Pr(>Chisq)`[2] %>% round(digits = 3)
+  coefs = gen_mod$coefficients
+  me <- (coefs['cwd.spstd'] + median_cwd * 2 * coefs['I(cwd.spstd^2)']) %>% round(digits = 3)
+  
+  # coef_table <- gen_mod %>% coeftable()
+  # coef <- coef_table[2,1]  %>% round(digits = 3)
+  # p <- coef_table[2, 4] %>% round(digits = 3)
+  n <- gen_mod$nobs
+  
+  label <-  paste0("   n sites: ", n, ";\n   slope: ", me, ";\n   p value: ", pvalue, "\n")
+  return(label)
+}
+
+
+coef_labels <- genus_df %>%
+  mutate(labels = map2(model_estimates, data, pull_coefs)) %>%
+  select(genus, labels) %>%
+  unnest(labels) %>%
+  arrange(genus)
+
+
+genus_predictions <- genus_df %>% 
+  mutate(predictions = map2(model_estimates, data, gen_marg_fx_df))
+
+genus_predictions <- genus_predictions %>% 
+  unnest(predictions) %>% 
+  select(-data, -model_estimates) %>% 
+  arrange(genus)
+
+gen_plot <- genus_predictions %>% 
+  # filter(genus %in% genus_keep) %>%
+  ggplot(aes(x = cwd.spstd)) + 
+  geom_line(aes(y = estimate)) +
+  geom_ribbon(aes(ymin=conf.low, ymax=conf.high), alpha=0.2) +
+  # geom_ribbon(aes(ymin=cwd_ci_min, ymax=cwd_ci_max), alpha=0.2, fill = "darkblue") +
+  facet_wrap(~genus, scales = "free", ncol = 3) +
+  theme(
+    # panel.grid.major = element_blank(),
+    # panel.grid.minor = element_blank(),
+    strip.background = element_blank(),
+    panel.border = element_rect(colour = "black", fill = NA)) +
+  geom_line(aes(y = conf.low), linetype = 3) +
+  geom_line(aes(y = conf.high), linetype = 3) +
+  geom_hline(yintercept = 0, linetype = 2) +
+  xlab("Historic CWD\n(Deviation from species mean)") + 
+  ylab("Predicted sensitivity to CWD") +
+  xlim(c(-3, 3))
+
+
+gen_plot <- gen_plot +
+  geom_text(data = coef_labels, aes(label = labels, x = -Inf, y = -Inf),
+            hjust = 0, vjust = 0)
+
+gen_plot
