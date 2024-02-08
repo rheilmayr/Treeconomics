6d682ef46c53d926d4ca5052c6662ec3e34ab111
fmoore125 <fmoore125@gmail.com>
2024-02-08 11:16:02 -0800

    extract terraclimate climatologies for itrdb sites

--- terradata_extraction.R
+++ terradata_extraction.R
@@ -3,6 +3,7 @@ library(raster)
 library(reshape2)
 library(terra)
 library(ncdf4)
+library(tidyr)
 wdir <- 'remote/'
 
 vars=c("def","pet","ppt")
@@ -30,3 +31,23 @@ for(var in vars){
   colnames(longdat)[2:3]=c("Month",var)
   fwrite(longdat,file=paste0("TerraClimate_Feb2024/itrdbsites_",var,".csv"))
 }
+
+climatologies=c("19611990","19812010")
+
+for(i in climatologies){
+  for (var in vars){
+    dat=terra::rast(paste0("TerraClimate_Feb2024/TerraClimate",i,"_",var,".nc"))
+    temp=extract(dat,lonlat,ID=FALSE)
+    colnames(temp)=1:12;temp$collection_id=locs$collection_id
+    if(var=="def"){
+      longdat=reshape2::melt(temp)
+      longdat$var=var
+      next
+    } 
+    temp=reshape2::melt(temp)
+    temp$var=var
+    longdat=rbind(longdat,temp)
+  }
+  fwrite(longdat,file=paste0("TerraClimate_Feb2024/itrdbsites_defpetppt_",i,".csv"))
+}
+
