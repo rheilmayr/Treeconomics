29627067a3e9b7ed3c35e0a546cbbad256d40fb4
fmoore125 <fmoore125@gmail.com>
2024-02-07 22:59:47 -0800

    extract terra climate annual data for itrdb sites

---
+++ terradata_extraction.R
@@ -0,0 +1,32 @@
+library(data.table)
+library(raster)
+library(reshape2)
+library(terra)
+library(ncdf4)
+wdir <- 'remote/'
+
+vars=c("def","pet","ppt")
+
+years=1958:2020
+
+locs=read.csv("out/dendro/site_summary.csv")
+lonlat=data.frame(lon=locs[,which(colnames(locs)=="longitude")],lat=locs[,which(colnames(locs)=="latitude")])
+
+for(var in vars){
+  for(i in years){
+    print(paste(var,i))
+    file=paste0("TerraClimate_Feb2024/",var,"/TerraClimate_",var,"_",i,".nc")
+    dat=terra::rast(file)
+    temp=extract(dat,lonlat,ID=FALSE)
+    colnames(temp)=1:12;temp$collection_id=locs$collection_id
+    if(i==1958){
+      longdat=reshape2::melt(temp)
+      longdat$year=i
+      next
+    }
+    temp=reshape2::melt(temp);temp$year=i
+    longdat=rbind(longdat,temp)
+  }
+  colnames(longdat)[2:3]=c("Month",var)
+  fwrite(longdat,file=paste0("TerraClimate_Feb2024/itrdbsites_",var,".csv"))
+}
