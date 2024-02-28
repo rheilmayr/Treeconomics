fe926e75a4e05a684fee409e312023f9b5ef9181
rheilmayr <rheilmayr@ucsb.edu>
2024-02-28 08:02:23 -0800

    Comapring TC, SPEI and our results

--- tc_second_stage.R
+++ tc_second_stage.R
@@ -30,9 +30,17 @@ wdir <- 'remote/'
 # 1. Site-level regressions
 flm_df <- read_csv(paste0(wdir, '2_output/first_stage/site_pet_cwd_std.csv'))
 flm_df_tc <- read_csv(paste0(wdir, '2_output/first_stage/site_pet_cwd_tc_std.csv'))
+flm_df_spei <- read_csv(paste0(wdir, '2_output/first_stage/site_pet_cwd_spei_std.csv'))
+flm_df_ppt <- read_csv(paste0(wdir, '2_output/first_stage/site_pet_ppt_std.csv'))
+flm_df_tc <- read_csv(paste0(wdir, '2_output/first_stage/site_pet_cwd_tc_std.csv'))
+
 
 flm_df <- flm_df_tc %>%
   select(collection_id, estimate_pet.an.spstd.tc, std.error_pet.an.spstd.tc, estimate_cwd.an.spstd.tc, std.error_cwd.an.spstd.tc) %>% 
+  inner_join(flm_df, by = "collection_id")
+
+flm_df <- flm_df_spei %>% 
+  select(collection_id, estimate_pet.an.spstd.spei, std.error_pet.an.spstd.spei, estimate_cwd.an.spstd.spei, std.error_cwd.an.spstd.spei) %>% 
   inner_join(flm_df, by = "collection_id")
 
 # 2. Historic site-level climate
@@ -68,11 +76,14 @@ flm_df <- flm_df %>%
 flm_df <- flm_df %>% 
   mutate(cwd_errorweights = 1 / (std.error_cwd.an),
          cwd_errorweights_tc = 1 / (std.error_cwd.an.spstd.tc),
+         cwd_errorweights_spei = 1 / (std.error_cwd.an.spstd.spei),
          errorweights2 = sqrt(ntrees),
          pet_errorweights = 1 / (std.error_pet.an.spstd.tc),
          int_errorweights = 1 / (std.error_intercept))
 
 # Identify and trim extreme outliers
+cwd_est_bounds.spei = quantile(flm_df$estimate_cwd.an.spstd.spei, c(0.01, 0.99),na.rm=T)
+pet_est_bounds.spei = quantile(flm_df$estimate_pet.an.spstd.spei, c(0.01, 0.99),na.rm=T)
 cwd_est_bounds.tc = quantile(flm_df$estimate_cwd.an.spstd.tc, c(0.01, 0.99),na.rm=T)
 pet_est_bounds.tc = quantile(flm_df$estimate_pet.an.spstd.tc, c(0.01, 0.99),na.rm=T)
 cwd_est_bounds = quantile(flm_df$estimate_cwd.an, c(0.01, 0.99),na.rm=T)
@@ -81,6 +92,8 @@ cwd_spstd_bounds = quantile(flm_df$cwd.spstd, c(0.01, 0.99), na.rm = T)
 pet_spstd_bounds = quantile(flm_df$pet.spstd, c(0.01, 0.99), na.rm = T)
 cwd_spstd_bounds_tc = quantile(flm_df$cwd.spstd.tc, c(0.01, 0.99), na.rm = T)
 pet_spstd_bounds_tc = quantile(flm_df$pet.spstd.tc, c(0.01, 0.99), na.rm = T)
+cwd_spstd_bounds_spei = quantile(flm_df$cwd.spstd.spei, c(0.01, 0.99), na.rm = T)
+pet_spstd_bounds_spei = quantile(flm_df$pet.spstd.spei, c(0.01, 0.99), na.rm = T)
 
 # flm_df <- flm_df %>%
 #   mutate(outlier = (estimate_cwd.an<cwd_est_bounds[1]) |
@@ -95,6 +108,18 @@ pet_spstd_bounds_tc = quantile(flm_df$pet.spstd.tc, c(0.01, 0.99), na.rm = T)
 
 flm_df <- flm_df %>%
   mutate(outlier = 
+           (estimate_cwd.an.spstd.spei<cwd_est_bounds.spei[1]) |
+           (estimate_cwd.an.spstd.spei>cwd_est_bounds.spei[2]) |
+           (estimate_pet.an.spstd.spei<pet_est_bounds.spei[1]) |
+           (estimate_pet.an.spstd.spei>pet_est_bounds.spei[2]) |
+           (cwd.spstd.spei<cwd_spstd_bounds_spei[1]) |
+           (cwd.spstd.spei>cwd_spstd_bounds_spei[2]) |
+           (pet.spstd.spei<pet_spstd_bounds_spei[1]) |
+           (pet.spstd.spei>pet_spstd_bounds_spei[2]))
+
+
+flm_df <- flm_df %>%
+  mutate(outlier = 
            (estimate_cwd.an<cwd_est_bounds[1]) |
            (estimate_cwd.an>cwd_est_bounds[2]) |
            (estimate_pet.an<pet_est_bounds[1]) |
@@ -103,6 +128,10 @@ flm_df <- flm_df %>%
            (estimate_cwd.an.spstd.tc>cwd_est_bounds.tc[2]) |
            (estimate_pet.an.spstd.tc<pet_est_bounds.tc[1]) |
            (estimate_pet.an.spstd.tc>pet_est_bounds.tc[2]) |
+           (estimate_cwd.an.spstd.spei<cwd_est_bounds.spei[1]) |
+           (estimate_cwd.an.spstd.spei>cwd_est_bounds.spei[2]) |
+           (estimate_pet.an.spstd.spei<pet_est_bounds.spei[1]) |
+           (estimate_pet.an.spstd.spei>pet_est_bounds.spei[2]) |
            (cwd.spstd<cwd_spstd_bounds[1]) |
            (cwd.spstd>cwd_spstd_bounds[2]) |
            (pet.spstd<pet_spstd_bounds[1]) |
@@ -112,8 +141,8 @@ flm_df <- flm_df %>%
            (pet.spstd.tc<pet_spstd_bounds_tc[1]) |
            (pet.spstd.tc>pet_spstd_bounds_tc[2]))
 
-# Save out full flm_df to simplify downstream scripts and ensure consistency
-flm_df %>% write.csv(paste0(wdir, "2_output/first_stage/site_pet_cwd_std_augmented.csv"))
+# # Save out full flm_df to simplify downstream scripts and ensure consistency
+# flm_df %>% write.csv(paste0(wdir, "2_output/first_stage/site_pet_cwd_std_augmented.csv"))
 
 # Trim outliers
 trim_df <- flm_df %>% 
@@ -140,6 +169,24 @@ cwd_mod <- feols(formula, data = mod_data, weights = mod_data$cwd_errorweights,
                  vcov = conley(cutoff = vg.range/1000, distance = "spherical"))
 summary(cwd_mod)
 
+formula = as.formula("estimate_cwd.an.tc ~ cwd.spstd.tc + (cwd.spstd.tc^2) + pet.spstd.tc + (pet.spstd.tc^2)")
+mod_data <- trim_df
+cwd_mod <- feols(formula, data = mod_data, weights = mod_data$cwd_errorweights,
+                 vcov = conley(cutoff = vg.range/1000, distance = "spherical"))
+summary(cwd_mod)
+
+formula = as.formula("estimate_cwd.an.spstd.tc ~ cwd.spstd + (cwd.spstd^2) + pet.spstd + (pet.spstd^2)")
+mod_data <- trim_df
+cwd_mod_tc <- feols(formula, data = mod_data, weights = mod_data$cwd_errorweights_tc,
+                    vcov = conley(cutoff = vg.range/1000, distance = "spherical"))
+summary(cwd_mod_tc)
+
+formula = as.formula("estimate_cwd.an.spstd.spei ~ cwd.spstd.spei + (cwd.spstd.spei^2) + pet.spstd.spei + (pet.spstd.spei^2)")
+mod_data <- trim_df
+cwd_mod <- feols(formula, data = mod_data, weights = mod_data$cwd_errorweights_spei,
+                 vcov = conley(cutoff = vg.range/1000, distance = "spherical"))
+summary(cwd_mod)
+
 
 formula = as.formula("estimate_cwd.an.spstd.tc ~ cwd.spstd.tc + (cwd.spstd.tc^2) + pet.spstd.tc + (pet.spstd.tc^2)")
 mod_data <- trim_df
@@ -151,7 +198,8 @@ summary(lm(estimate_cwd.an.spstd.tc ~ estimate_cwd.an, data = trim_df))
 trim_df %>% 
   ggplot(aes(x = estimate_cwd.an, y = estimate_cwd.an.spstd.tc)) +
   geom_point(alpha = 0.5) +
-  geom_abline(slope = 1, intercept = 0)
+  geom_abline(slope = 1, intercept = 0) +
+  geom_smooth()
 
 
 summary(lm(cwd.spstd.tc ~ cwd.spstd, data = trim_df))
